546b08299035a09eb294c0fd8f6c9d55ebac8366
Anthonios Partheniou <partheniou@google.com>
2025-03-13 08:55:00 -0400

    fix(deps): allow Protobuf 6.x

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ dependencies = [
     "click",
     "colorlog",
     "google-cloud-storage<4.0.0dev",
-    "protobuf>=3.20.2,<6.0.0dev,!=4.21.0,!=4.21.1,!=4.21.2,!=4.21.3,!=4.21.4,!=4.21.5",
+    "protobuf>=3.20.2,<7.0.0dev,!=4.21.0,!=4.21.1,!=4.21.2,!=4.21.3,!=4.21.4,!=4.21.5",
 ]
 
 packages = setuptools.find_packages()
